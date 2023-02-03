a81c294e29086b438008c7c190f119439238d932
wtno <920093991@qq.com>
2023-02-03 15:26:43 +0800

    23. Delegatecall

--- learning_advanced/23_ Delegatecall.sol
+++ learning_advanced/23_ Delegatecall.sol
@@ -5,6 +5,7 @@ pragma solidity ^0.8.4;
  * delegatecall 和 call 区别
  * 1.delegatecall在调用合约时可以指定交易发送的gas，但不能指定发送的ETH数额
  * 2.第二点有点类似前端的转发和重定向？
+ * 不同，应该说是调用别人的方法，改变的数据是自身的状态变量（调用者和被调用者的状态变量和定义顺序需要相同（貌似有关））
  *
  * PS：delegatecall有安全隐患，使用时要保证《当前合约》和《目标合约》的《状态变量存储结构》相同，
  *     并且目标合约安全，不然会造成资产损失。
@@ -15,6 +16,14 @@ pragma solidity ^0.8.4;
  * 2.EIP-2535 Diamonds（钻石）:钻石是一个支持构建可在生产中扩展的模块化智能合约系统的标准。钻石是具有多个实施合同的代理合同。
  */
 contract Delegatecall {
+    
+}
+
+// 代理合约
+contract B {
+    uint public num;
+    address public sender;
+
     // 使用call调用合约C的setVars函数，改变的是合约C中的状态变量num
     function callSetVars(address _address, uint256 _num) public payable {
         (bool success, bytes memory data) = _address.call(abi.encodeWithSignature("setVars(uint256)", _num));
@@ -24,12 +33,6 @@ contract Delegatecall {
     function delegatecallSetVars(address _address, uint256 _num) public payable {
         (bool success, bytes memory data) = _address.delegatecall(abi.encodeWithSignature("setVars(uint256)", _num));
     }
-}
-
-// 代理合约
-contract B {
-    uint public num;
-    address public sender;
 }
 
 // 逻辑合约
