dd468fc98e5ee997fcfb202cdeecc597d40ef92f
wtno <920093991@qq.com>
2023-02-03 15:14:18 +0800

    23. Delegatecall

---
+++ learning_advanced/23_ Delegatecall.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+/**
+ * delegatecall 和 call 区别
+ * 1.delegatecall在调用合约时可以指定交易发送的gas，但不能指定发送的ETH数额
+ * 2.第二点有点类似前端的转发和重定向？
+ *
+ * PS：delegatecall有安全隐患，使用时要保证《当前合约》和《目标合约》的《状态变量存储结构》相同，
+ *     并且目标合约安全，不然会造成资产损失。
+ * 
+ * 使用场景
+ * 1.代理合约:将智能合约的存储合约和逻辑合约分开，代理合约（Proxy Contract）存储所有相关的变量，并且保存逻辑合约的地址；
+ *           所有函数存在逻辑合约（Logic Contract）里，通过delegatecall执行。当升级时，只需要将代理合约指向新的逻辑合约即可。
+ * 2.EIP-2535 Diamonds（钻石）:钻石是一个支持构建可在生产中扩展的模块化智能合约系统的标准。钻石是具有多个实施合同的代理合同。
+ */
+contract Delegatecall {
+    // 使用call调用合约C的setVars函数，改变的是合约C中的状态变量num
+    function callSetVars(address _address, uint256 _num) public payable {
+        (bool success, bytes memory data) = _address.call(abi.encodeWithSignature("setVars(uint256)", _num));
+    }
+
+    // 使用call调用合约C的setVars函数，改变的是合约C中的状态变量num
+    function delegatecallSetVars(address _address, uint256 _num) public payable {
+        (bool success, bytes memory data) = _address.delegatecall(abi.encodeWithSignature("setVars(uint256)", _num));
+    }
+}
+
+// 代理合约
+contract B {
+    uint public num;
+    address public sender;
+}
+
+// 逻辑合约
+contract C {
+    uint public num;
+    address public sender;
+
+    function setVars(uint _num) public payable {
+        num = _num;
+        sender = msg.sender;
+    }
+}
--- learning_advanced/Test.sol
+++ learning_advanced/Test.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.6.2 <0.9.0;
 contract Fund {
     mapping(address => uint) shares;
     function withdraw() public {
-        if (payable(msg.sender).call.value(shares[msg.sender])())
-            shares[msg.sender] = 0;
+        // if (payable(msg.sender).call.value(shares[msg.sender])())
+        //     shares[msg.sender] = 0;
     }
 }
